b149441ec7456837532df5760dd7f240922826e0
amolika1614 <yadavamolika12@gmail.com>
2026-05-16 11:45:32 +0530

    Array

---
+++ Find_minimum_rotated_sorted_array.java
@@ -0,0 +1,38 @@
+/*Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,4,4,5,6,7] might become:
+
+[4,5,6,7,0,1,4] if it was rotated 4 times.
+[0,1,4,4,5,6,7] if it was rotated 7 times.
+Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+
+Given the sorted rotated array nums that may contain duplicates, return the minimum element of this array.
+
+You must decrease the overall operation steps as much as possible.*/
+class Solution {
+    public static int findMin(int[] nums) {
+        int n = nums.length - 1;
+        int last = nums[n];
+        int left = 0, right = n;
+
+        while (left < n && nums[left] == last)
+            left++;
+
+        while (left < right) {
+            int mid = left + right >> 1;
+
+            if (nums[mid] > last)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+
+        return nums[left];
+    }
+public static void main(String gg[])
+{
+     int [] arr={3,1,2};
+
+  int n= findMin(arr);
+ System.out.println(n);
+}
+
+}
