1151099c2c85c6dd84545bc49e5c3749bbf4670c
amolika1614 <yadavamolika12@gmail.com>
2026-07-01 20:09:25 +0530

    Array

--- merge_sorted.java
+++ merge_sorted.java
@@ -10,7 +10,7 @@ class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n)
     {  
         for(int i=0 ; i<n ; i++)
-        {
+        {s
             nums1[m]=nums2[i];
             m++;
         }
